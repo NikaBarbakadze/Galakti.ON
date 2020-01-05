afd22fa848e383d09679c61d6a9c3f57e2ffd38e
NickOldman <39439578+NickOldman@users.noreply.github.com>
2020-01-05 16:46:36 -0500

    main file fix

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Galakti.ON</title>
+<link rel="shortcut icon" href="<?php // echo base_url(); ?>markup/themes/images/galaktion.svg">
+<meta name="theme-color" content="#000">
+<meta name="msapplication-navbutton-color" content="#000">
+<meta name="apple-mobile-web-app-status-bar-style" content="#000">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="markup/scripts/css/main.css">
+<script type="text/javascript" src="markup/scripts/js/main.js"></script>
+</head>
+<body>
+
+	<?php include("markup/blocks/header.php"); ?>
+
+	<?php include("markup/blocks/first_section.php"); ?>
+
+	<?php include("markup/blocks/second_section.php"); ?>
+
+	<?php include("markup/blocks/footer.php"); ?>
+
+</body>
+</html>
